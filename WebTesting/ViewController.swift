6787c0fc1977a83e07960bd16d24e0f5c5eb9f49
Apple Coding <academy@applecoding.com>
2019-04-05 10:58:19 +0200

    Initial Commit

---
+++ WebTesting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebTesting
+//
+//  Created by Dev1 on 05/04/2019.
+//  Copyright © 2019 Dev1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
